8679535f7d40595aeb919d3189c795acbcb5219e
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-11 19:34:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 class Student
   
   def hello
-    "Hey there! I\'m so excited to learn stuff."
+    puts "Hey there! I\'m so excited to learn stuff."
+  end 
+  
+  def raise_hand 
+    puts "Pick me!"
   end 
 end 
